7d2236be22599e1a740a74e54695cbd442c85e2d
isaiah <isaiah.v@comcast.net>
2023-12-14 12:18:54 -0800

    added the liquibase scripts to the docker container

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+pushd /database || exit 1;
+/liquibase/liquibase \
+  --changelog-file "org/ivcode/homeportal/changelog/db.changelog-root.yml" \
+  --url $DATABASE_URL \
+  --username $DATABASE_USERNAME \
+  --password $DATABASE_PASSWORD \
+  update || exit 1;
+popd || exit 1;
+
 java -D \
  -Dspring.servlet.multipart.max-file-size=100MB \
  -Dspring.servlet.multipart.max-request-size=100MB \
