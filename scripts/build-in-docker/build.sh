f29a2c59838d0dcce2a496f93d76ebb8f49aab02
isaiah <isaiah.v@comcast.net>
2023-12-16 13:30:15 -0800

    setting build version is now

--- scripts/build-in-docker/build.sh
+++ scripts/build-in-docker/build.sh
@@ -2,4 +2,9 @@
 
 cd $(dirname "$(realpath $0)")
 cd ../../
-docker run --rm --workdir /build -v ${PWD}/:/build home-portal-build
+docker run \
+  --rm \
+  --workdir /build \
+  -v ${PWD}/:/build \
+  -e npm_package_version=${npm_package_version} \
+  home-portal-build
